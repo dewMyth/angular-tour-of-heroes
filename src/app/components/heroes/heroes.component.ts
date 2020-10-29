90bbd51770a8f759ac606664fb01004fe47fee2a
dewMyth <akalankadewmith@gmail.com>
2020-10-29 11:10:51 +0530

    add click hero message

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { MessageService } from 'src/app/services/message.service';
 import { Hero } from '../../models/Hero';
 import { HeroService } from '../../services/hero.service';
 
@@ -11,7 +12,10 @@ import { HeroService } from '../../services/hero.service';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[]; 
-  constructor(private heroService : HeroService) { }
+  selectedHero : Hero;
+
+
+  constructor(private heroService : HeroService, private messageService : MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -21,10 +25,11 @@ export class HeroesComponent implements OnInit {
    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
-  selectedHero : Hero;
+
   
   onSelect(hero : Hero) : void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
 }
