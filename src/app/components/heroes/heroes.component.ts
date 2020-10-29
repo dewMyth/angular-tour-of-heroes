04011aae8902d36a29f40dfa2f5a16ef5a5865c4
dewMyth <akalankadewmith@gmail.com>
2020-10-29 15:00:58 +0530

    Add routing to app

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -12,7 +12,6 @@ import { HeroService } from '../../services/hero.service';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[]; 
-  selectedHero : Hero;
 
 
   constructor(private heroService : HeroService, private messageService : MessageService) { }
@@ -28,7 +27,7 @@ export class HeroesComponent implements OnInit {
 
   
   onSelect(hero : Hero) : void {
-    this.selectedHero = hero;
+
     this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
